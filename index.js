b895ff43dd530145cf096b6d59530ed184ed1c2b
yyunqingchenn <yyunqingchenn@gmail.com>
2019-06-28 03:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(string)
   console.log(string.toLowerCase())
 }
 
-function sayHitoGrandma()
+function sayHitoGrandma(string)
 {
   if (string === string.toLowerCase())
     return "I can't hear you!";
