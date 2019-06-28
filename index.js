914cd19180f90012999d46db522831336db0a015
yyunqingchenn <yyunqingchenn@gmail.com>
2019-06-28 03:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ function sayHitoGrandma(string)
   else if (string = string.toUpperCase()),
     return "YES INDEED!";
   else if (string === "I love you, Grandma."),
-    return "I love you, too."
+    return "I love you, too.";
+}
