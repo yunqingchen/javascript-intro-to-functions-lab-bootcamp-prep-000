484744fa1b3b4b41ceca53eff2c67a97fd4a330d
yyunqingchenn <yyunqingchenn@gmail.com>
2019-06-28 03:20:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function sayHitoGrandma(string)
 {
   if (string === string.toLowerCase())
     return "I can't hear you!";
-  else if (string === string.toUpperCase())
-    return "YES INDEED!";
   else if (string === "I love you, Grandma.")
     return "I love you, too.";
+  else if (string === string.toUpperCase())
+    return "YES INDEED!";
 }
